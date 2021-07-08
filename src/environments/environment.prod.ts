255fe929891477f4bd52ac1c6de9587ffe8a2b93
andres-hub <andtrescalderoncl@gmail.com>
2021-07-08 15:37:03 -0500

    fin de modelado del alcance del proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 //TODO: Combiar url de produccion
 export const environment = {
   production: true,
-  base_url: 'https://finanzas-back-end.herokuapp.com/api',
-  nombreApi: 'Wallet App'
+  base_url: 'https://facturacion-inventario.herokuapp.com/api',
+  nombreApi: 'Facturacion & Inventario'
 
 };
